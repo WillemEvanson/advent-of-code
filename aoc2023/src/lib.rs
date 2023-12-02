728cd16fa3eb14eaba8b4d37ffefacde1454f15b
Willem Evanson <willemevanson@gmail.com>
2023-12-01 23:25:52 -0600

    2023/2 - Add solution

--- aoc2023/src/lib.rs
+++ aoc2023/src/lib.rs
@@ -1,2 +1,3 @@
 #![feature(ascii_char)]
 pub mod day1;
+pub mod day2;
--- aoc2023/src/main.rs
+++ aoc2023/src/main.rs
@@ -3,6 +3,7 @@ use aoc2023::*;
 #[rustfmt::skip]
 const FUNCTIONS: &[fn(&str) -> util::Solution] = &[
     day1::solve,
+    day2::solve,
 ];
 
 fn main() {

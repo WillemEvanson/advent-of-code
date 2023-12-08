a8e1bfbdb3584244734aa87032e8bb0b936f17e9
Willem Evanson <willemevanson@gmail.com>
2023-12-07 23:52:55 -0600

    Util - Add lcm and gcd functions

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -1,3 +1,19 @@
 mod solution;
 
 pub use solution::{Answer, Solution};
+
+pub fn gcd(a: u64, b: u64) -> u64 {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
+pub fn lcm(a: u64, b: u64) -> u64 {
+    if a > b {
+        (a / gcd(a, b)) * b
+    } else {
+        (b / gcd(a, b)) * a
+    }
+}

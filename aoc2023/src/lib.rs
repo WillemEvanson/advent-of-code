61926cdb7d4dafd10f9772e6a043b3e002942862
Willem Evanson <willemevanson@gmail.com>
2023-12-18 01:40:05 -0500

    2023/18 - Add solution

--- aoc2023/src/lib.rs
+++ aoc2023/src/lib.rs
@@ -1,4 +1,4 @@
-#![feature(ascii_char, iter_array_chunks)]
+#![feature(array_windows, ascii_char, iter_array_chunks)]
 pub mod day1;
 pub mod day10;
 pub mod day11;
@@ -8,6 +8,7 @@ pub mod day14;
 pub mod day15;
 pub mod day16;
 pub mod day17;
+pub mod day18;
 pub mod day2;
 pub mod day3;
 pub mod day4;

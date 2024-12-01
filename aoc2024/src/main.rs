dc98897cf3150891d49b38f17e0cc247dee3de61
Willem Evanson <willemevanson@gmail.com>
2024-11-30 18:50:37 -0600

    Add Advent of Code 2024

---
+++ aoc2024/src/main.rs
@@ -0,0 +1,24 @@
+const FUNCTIONS: &[fn(&str) -> util::Solution] = &[];
+
+fn main() {
+    let Some(input_dir) = std::env::args_os().nth(1) else {
+        eprintln!("aoc2024 [input dir]");
+        return;
+    };
+
+    let path = std::path::Path::new(&input_dir);
+    let mut total_duration = std::time::Duration::default();
+    for i in 1..=FUNCTIONS.len() {
+        let path = path.join(format!("day{i}.txt"));
+        if let Ok(input) = std::fs::read_to_string(&path) {
+            let now = std::time::Instant::now();
+            let solution = FUNCTIONS[i - 1](&input);
+            let elapsed = now.elapsed();
+            total_duration += elapsed;
+
+            println!("Day {i}:\t{solution}{elapsed:?}");
+        }
+    }
+
+    println!("Total:\t{:<40}{total_duration:?}", "");
+}

e6cf306b7ef5e124b34cc9427884a908502902fe
Willem Evanson <willemevanson@gmail.com>
2024-12-05 17:53:41 -0600

    Util - Add UnionFind struct

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -2,11 +2,13 @@ mod bit_set;
 mod lexer;
 mod random;
 mod solution;
+mod union_find;
 
 pub use bit_set::BitSet;
 pub use lexer::Lexer;
 pub use random::Rng;
 pub use solution::{Answer, Solution};
+pub use union_find::UnionFind;
 
 pub fn gcd(a: u64, b: u64) -> u64 {
     if b == 0 {

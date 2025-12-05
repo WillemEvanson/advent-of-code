ef9a8e9e67726a5a909ec2baba38f2b079ac72d9
Willem Evanson <willemevanson@gmail.com>
2025-12-05 01:33:07 -0600

    2025/2 - Use intervals to accelerate search.
    By splitting each range into intervals where the start and end values
    have the same number of digits, we can further segment those intervals
    into equal-sized digit groups (of any valid group size). This lets us
    check only the digit groups that fall within the range of the most
    significant segment.
    
    By tracking whether we are constrained by the upper or lower bound
    (i.e., whether all previous segments were at the top or bottom of their
    respective ranges), we can determine validity: any segment that falls
    outside the allowable range and is constrained in the direction it
    violates is not a valid product range.

--- aoc2025/src/day02.rs
+++ aoc2025/src/day02.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use util::Solution;
 
 pub fn solve(input: &str) -> Solution {
@@ -28,31 +30,84 @@ pub fn solve(input: &str) -> Solution {
         let first = first.parse::<u64>().unwrap();
         let second = second.parse::<u64>().unwrap();
 
-        for i in first..=second {
-            let log = i.ilog10();
-            for splits in 1..=log {
-                let power = (log + 1) / (splits + 1);
-                if (log + 1) % (splits + 1) == 0 {
-                    let start = i % (10u64.pow(power));
+        // We turn this into a set of intervals with start and end having the same
+        // number of digits.
+        let first_digits = first.ilog10() + 1;
+        let second_digits = second.ilog10() + 1;
+
+        let mut digits = first_digits;
+        let mut found = HashSet::new();
+        loop {
+            let min_tens = 10u64.pow(digits - 1);
+            let max_tens = min_tens * 10;
+
+            let start = u64::max(min_tens, first);
+            let end = u64::min(max_tens - 1, second);
+
+            for splits in 1..digits {
+                // Check if it can't be split into equal-sized groups
+                if digits % (splits + 1) != 0 {
+                    continue;
+                }
+
+                let power = digits / (splits + 1);
+                let to_power = 10u64.pow(power);
+
+                // Segment the digits of the top
+                let mut cur = end;
+                let mut top = vec![0; splits as usize + 1];
+                for i in (0..=splits as usize).rev() {
+                    let next = cur % to_power;
+                    top[i] = next;
+                    cur /= to_power;
+                }
+
+                // Segment the digits of the bottom
+                let mut cur = start;
+                let mut bottom = vec![0; splits as usize + 1];
+                for i in (0..=splits as usize).rev() {
+                    let next = cur % to_power;
+                    bottom[i] = next;
+                    cur /= to_power;
+                }
+
+                // Determine whether each starting segment results in an invalid ID.
+                for j in bottom[0]..=top[0] {
+                    let mut free_bottom = bottom[0] != j;
+                    let mut free_top = top[0] != j;
 
                     let mut valid = true;
-                    let mut cur = i / (10u64.pow(power));
-                    for _ in 0..splits {
-                        let next = cur % (10u64.pow(power));
-                        if next != start {
+                    for i in 1..bottom.len() {
+                        if !free_bottom && j < bottom[i] {
+                            valid = false;
+                            break;
+                        } else if !free_top && j > top[i] {
                             valid = false;
                             break;
                         }
-                        cur = cur / (10u64.pow(power));
+
+                        free_bottom = free_bottom || bottom[i] < j;
+                        free_top = free_top || top[i] > j;
                     }
 
                     if valid {
-                        part2 += i;
-                        break;
-                    } else {
+                        let number = (0..splits as u64).fold(j, |accum, _| accum * to_power + j);
+
+                        if found.insert(number) {
+                            if splits == 1 {
+                                part1 += number;
+                            }
+
+                            part2 += number;
+                        }
                     }
                 }
             }
+
+            if digits == second_digits {
+                break;
+            }
+            digits += 1;
         }
     }
 
